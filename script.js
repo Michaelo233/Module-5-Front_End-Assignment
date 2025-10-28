f192dc4c769f9afc17a1e16a394d4b3be1b4837f
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-10-28 10:06:05 -0500

    feat: constructed and fetch api url request using async function.

---
+++ script.js
@@ -0,0 +1,8 @@
+// fetch api for tree inventory
+
+async function getTreeInventory(search) {
+    const response = await fetch("https://data.winnipeg.ca/api/v3/views/hfwk-jp4h/query.json");
+    if (!response.ok) {
+        throw new Error(`HTTP Error! status: ${response.status}`);
+    }
+}
