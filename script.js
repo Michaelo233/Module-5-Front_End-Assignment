313e9e57f606a30a9c407986353ac7328bd958c1
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-11-08 16:52:53 -0600

    feat and fix: created a display function for the api, and also made some fixes to my api fetch.

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-// fetch api for tree inventory
-
 async function getTreeInventory(search) {
-    const response = await fetch("https://data.winnipeg.ca/api/v3/views/hfwk-jp4h/query.json");
-    if (!response.ok) {
+    
+    const apiUrl = `https://data.winnipeg.ca/resource/hfwk-jp4h.json?` + 
+                    `$where=common_name LIKE '%${treeInput.value}%'` + 
+                    '&$order=diameter_at_breast_height DESC' +
+                    '&$limit=100';
+    const encodedURL = encodeURI(apiUrl);
+    
+    const response = await fetch(encodedURL);
+    try {
+        
+        if (!response.ok) {
         throw new Error(`HTTP Error! status: ${response.status}`);
+        }
+
+        // Parse and return
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+}
+
+async function displayTreeInventory() {
+    try {
+        const data = await getTreeInventory();
+        display.innerHTML = '';
+        data.forEach(tree => {
+            
+            const treeTable = Object.entries(tree).map(item => ({
+                treeID: item[0],
+                commonName: item[2],
+                botanicalName: item[1],
+                neighbourhood: item[4]
+            }));
+            console.log(tree)
+            console.table(treeTable.slice(0, 20));
+            
+
+        });
+
+
+
+    } catch (error) {
+        console.error("Failed to display data:", error);
     }
 
-    // Parse and return
-    const data = await response.json();
-    return data;
 }
